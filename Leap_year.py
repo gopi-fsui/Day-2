da59b3c2bd52f7d523b90b395a3e9a589c5d699a
gopi-fsui <gopi.fsui@gmail.com>
2026-02-18 23:58:25 +0530

    Update Leap_year.py

--- Leap_year.py
+++ Leap_year.py
@@ -14,4 +14,3 @@ def is_leap_year(year):
     return False
     # given by ai(mind-blowing!!!)
     # return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0) 
-    # return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0) 
